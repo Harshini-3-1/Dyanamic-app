5e2bb2b95b61050855b541d5c253a0e51bb1e323
Harshini-3-1 <157867707+Harshini-3-1@users.noreply.github.com>
2024-06-21 15:32:44 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Weather Dashboard</h1>
+      <p>Displaying current weather data and trends</p>
+    </header>
+  );
+};
+
+export default Header;
